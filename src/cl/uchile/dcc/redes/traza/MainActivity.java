87ecac57b0dd0f3a336a69aad31c3e53338045c7
Jaime Carrera <dongraft@gmail.com>
2014-06-02 22:53:47 -0400

    MensajeTrazando
    En MainActivity hay que iniciar el demonio que traza y hace la magia

--- src/cl/uchile/dcc/redes/traza/MainActivity.java
+++ src/cl/uchile/dcc/redes/traza/MainActivity.java
@@ -1,17 +1,19 @@
 package cl.uchile.dcc.redes.traza;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	public final static String EXTRA_MESSAGE = "cl.uchile.dcc.redes.traza.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +66,17 @@ public class MainActivity extends Activity {
     
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-        // Do something in response to button
+    	//Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	
+    	// Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(40);
+	    textView.setText("Trazando");
+
+	    // Set the text view as the activity layout
+	    setContentView(textView);
+    	
+        //startActivity(intent);
     }
 
 }
